69b51ef66f36aa914f648803414422ac90e4234b
ohsangsoo <tkdtn3264@gmail.com>
2023-07-19 20:20:57 +0900

    삭제 기능 로직을 nullable로 변경

--- lib/domain/repository/note_repostiory.dart
+++ lib/domain/repository/note_repostiory.dart
@@ -4,7 +4,7 @@ abstract class NoteRepository {
   Future<List<Note>> getNoes();
 
   // 클릭 하면 해당하는 메모페이지 로이동 을 아이디로 하겠다.
-  Future<Note> getNoteById(int id);
+  Future<Note?> getNoteById(int id);
   // 노트 추가
   Future<void> insertNote(Note note);
   // 노트 수정
