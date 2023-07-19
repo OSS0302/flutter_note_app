d71d83ab4e0195ffbafc2e8d73e24f2a91d85c90
ohsangsoo <tkdtn3264@gmail.com>
2023-07-19 19:57:14 +0900

    노트 작성하기

--- lib/data/data_source/note_db.dart
+++ lib/data/data_source/note_db.dart
@@ -9,22 +9,28 @@ class NoteDb {
 
   Future<Note?> getNoteById(int id) async {
     // Select * from note where id = 1;
-    final List<Map<String ,dynamic>> maps = await db.query( // Json타입을 String ,dynamic 하겠다.
+    final List<Map<String, dynamic>> maps = await db.query(
+      // Json타입을 String ,dynamic 하겠다.
       'note',
       where: 'id = ?',
       whereArgs: [id],
     );
-    if (maps.isNotEmpty){ // maps 에 데이터 가 있다면
-      return Note.fromJson(maps.first); // 데이터 가 하나만 들어있기 때문에  maps 를 노트로변환해서 리턴한다.
+    if (maps.isNotEmpty) {
+      // maps 에 데이터 가 있다면
+      return Note.fromJson(
+          maps.first); // 데이터 가 하나만 들어있기 때문에  maps 를 노트로변환해서 리턴한다.
     }
     return null; // 데이터 없으면 null 로 반환한다.
   }
-  // 전체 노트 가져오기
-Future<List<Note>> getNotes( )async{
+
+  // 전체 노트 데이터 가져오기
+  Future<List<Note>> getNotes() async {
     final maps = await db.query('notes'); // 아무조건이 없으면 notes 모든 데이터 를 가져오겠다.
     return maps.map((e) => Note.fromJson(e)).toList(); //노트를 리스트형태로
-}
+  }
 
-
-
+  // 노트 추가히기
+  Future<void> insertNote(Note note) async {
+    await db.insert('note', note.toJson()); //  note 테이블  map Json 데이터를 전달
+  }
 }
