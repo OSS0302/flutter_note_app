9450c77fc065b55264c75a7d98f1a370179fab30
ohsangsoo <tkdtn3264@gmail.com>
2023-07-20 17:48:41 +0900

    required id 를 널 있을수도 있어서 변경

--- lib/domain/model/note.dart
+++ lib/domain/model/note.dart
@@ -14,7 +14,7 @@
    required String content,// 내용
    required int color, // 노트 컬러
    required int timestamp, // 시간 있어야  최근에 올린 노트를 확인이 가능하다.
-   required int? id, //  데이터 베이스 값을 활용하기 위해서 아이디 가 필요하다.
+    int? id, //  데이터 베이스 값을 활용하기 위해서 아이디 가 필요하다.
     }) = _Note;
 
     factory Note.fromJson(Map<String, dynamic> json) => _$NoteFromJson(json);
