e4054cb9393b24db49ebcd1b2c7fb5c4672e8c92
ohsangsoo <tkdtn3264@gmail.com>
2023-07-20 17:17:48 +0900

    // 모든 노트를 불러오는 기능

---
+++ lib/domain/use_case/get_notes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/repository/note_repostiory.dart';
+
+// 모든 노트를 불러오는 기능
+class GetNotes {
+  final NoteRepository repository; // NoteRepository를 활용해야되서
+
+  GetNotes(this.repository);// 생성자를 받아서 사용을 합니다.
+
+  Future<List<Note>> call() async{
+    List<Note> notes = await repository.getNoes();
+    return notes;
+
+  }
+}
