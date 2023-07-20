1949978170a2c247653b64a3bd61b3328bd70ad8
ohsangsoo <tkdtn3264@gmail.com>
2023-07-20 17:56:40 +0900

    노트 한개 상세보는 기능

---
+++ lib/domain/use_case/get_Note.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/repository/note_repostiory.dart';
+
+class GetNote{
+  final NoteRepository repository;
+
+  GetNote(this.repository);
+
+  Future<Note?> call(int id) async{
+    return await repository.getNoteById(id); // 입출력 아이디 가 동일하기 때문이다.
+  }
+}
