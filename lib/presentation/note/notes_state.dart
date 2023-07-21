95fb1575fce328f77783f9ac1d69c6837a6cfea2
ohsangsoo <tkdtn3264@gmail.com>
2023-07-21 21:17:47 +0900

    freezed 한후 빌드하기

---
+++ lib/presentation/note/notes_state.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'notes_state.freezed.dart';
+
+part 'notes_state.g.dart';
+
+@freezed
+class NotesState with _$NotesState {
+  factory NotesState({
+    @Default([]) List<Note> notes, // List<Note> notes =[] 를 줄 수없어서 @Default를 사용해서 넣었다.
+  }) = _NotesState;
+
+  factory NotesState.fromJson(Map<String, dynamic> json) => _$NotesStateFromJson(json);
+}
