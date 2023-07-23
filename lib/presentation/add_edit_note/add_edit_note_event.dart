162425bf5305a4b36dc316a89b1cabb092375218
ohsangsoo <tkdtn3264@gmail.com>
2023-07-23 20:57:45 +0900

    두번째 화면 이벤트 freezed 생성 후 빌드 하기

--- lib/presentation/add_edit_note/add_edit_note_event.dart
+++ lib/presentation/add_edit_note/add_edit_note_event.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'add_edit_note_event.freezed.dart';
 
 @freezed
-abstract class AddEditNoteEvent<T> with _$AddEditNoteEvent<T> {
+abstract class AddEditNoteEvent with _$AddEditNoteEvent {
   const factory AddEditNoteEvent.changeColor(int color) = ChangeColor; //색깔의 컬러 를 int로 db 에 저장하겠다.
   const factory AddEditNoteEvent.saveNote(int? id , String title, String content) =saveNote; // 원시타입으로 아이디가 널 있을수도 있어서 ? 넣고 제목 과 내용 String으로 한다.
 }
