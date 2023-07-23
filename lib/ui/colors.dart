5a2b848f272a4dbe4db2d8209d03a427426b2ab5
ohsangsoo <tkdtn3264@gmail.com>
2023-07-23 21:45:45 +0900

    두번째 화면 에 들어갈 컬러 클래스

---
+++ lib/ui/colors.dart
@@ -0,0 +1,12 @@
+
+import 'dart:ui';
+// 메모 컬러 변경할 5가지 색상
+const roseBud = Color(0xFFFFAB91);
+const primrose = Color(0xFFE7ED9B);
+const wisteria = Color(0xFFCF94DA);
+const skyBlue = Color(0xFF81DEEA);
+const illusion = Color(0xFFF48FB1);
+
+// 메모앱 배경색 과 추가 버튼 과 앱 제목 컬러
+const dartGary = Color(0xff202020); // 앱 배경색
+const lightBlue =Color(0xFFD7E8DE); // 앱 타이틀색 과 버튼 색
