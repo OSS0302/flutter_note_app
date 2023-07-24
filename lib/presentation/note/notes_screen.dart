ce947ceb7f59377bc0066a8a0306c6bf40f4c8e6
ohsangsoo <tkdtn3264@gmail.com>
2023-07-24 19:45:19 +0900

    제목 타이틀 Ui

--- lib/presentation/note/notes_screen.dart
+++ lib/presentation/note/notes_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/presentation/note/components/note_item.dart';
+import 'package:flutter_note_app/ui/colors.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({Key? key}) : super(key: key);
@@ -19,16 +22,35 @@ class NotesScreen extends StatelessWidget {
         ],
         elevation: 0, // elevation을 0 으로 두면 appbar 도 다크 그레이 가된다.
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
       // container 안에 리스트를 통해서 모든 노트 가져오기
-      body: ListView(
-
-
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            NoteItem(
+                note: Note(
+                    title: 'title 1',
+                    content: 'content 1',
+                    color: wisteria .value,
+                    timestamp: 1
+                )
+            ),
+            NoteItem(
+                note: Note(
+                    title: 'title 2',
+                    content: 'content 2',
+                    color: skyBlue .value,
+                    timestamp: 2
+                )
+            ),
+          ],
+        ),
       ),
-
     );
   }
 }
