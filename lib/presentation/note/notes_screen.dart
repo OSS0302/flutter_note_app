44d1f28d1bd50905ff7a1b7d016d6718232d3364
ohsangsoo <tkdtn3264@gmail.com>
2023-07-24 14:47:58 +0900

    appbar 아이콘 sort 버튼 추가

--- lib/presentation/note/notes_screen.dart
+++ lib/presentation/note/notes_screen.dart
@@ -5,11 +5,30 @@ class NotesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {} ,
-        child: Icon(Icons.add),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '나의 노트',
+          style: TextStyle(fontSize: 30),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.sort), // appbar 상단에 솔트 버튼 추가
+          )
+        ],
+        elevation: 0, // elevation을 0 으로 두면 appbar 도 다크 그레이 가된다.
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      // container 안에 리스트를 통해서 모든 노트 가져오기
+      body: ListView(
+
+
+      ),
+
     );
   }
 }
