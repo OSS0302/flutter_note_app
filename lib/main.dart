2b7561194089a19d6146f26aa4fd305ca68a3ade
ohsangsoo <tkdtn3264@gmail.com>
2023-07-25 18:15:03 +0900

    화면 그리기 이전에 세팅 되는 과정들 그런 것들 보장하고 나서 화면을 그린다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/di/provider_setup.dart';
 import 'package:flutter_note_app/presentation/note/notes_screen.dart';
 import 'package:flutter_note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //db 가 네이티브 내부에서 사용하면서 플랫폼 바인딩을 보장한다.
+  final providers = await getProvider(); //프로바이더를 가져오고
+
+
+  runApp(
+    MultiProvider(
+      providers: providers,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,19 +31,16 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: darkGary,
         canvasColor: darkGary,
         floatingActionButtonTheme:
-          Theme.of(context).floatingActionButtonTheme.copyWith(
-            backgroundColor:Colors.white,  // 추가버튼를 흰색
-            foregroundColor: darkGary, // + 버튼 색 다크 그래이로 변경
-          ),
-          // appbar 테마 설정
-          appBarTheme:
-          Theme.of(context).appBarTheme.copyWith(
-            backgroundColor: darkGary,
-        ),
-
+            Theme.of(context).floatingActionButtonTheme.copyWith(
+                  backgroundColor: Colors.white, // 추가버튼를 흰색
+                  foregroundColor: darkGary, // + 버튼 색 다크 그래이로 변경
+                ),
+        // appbar 테마 설정
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+              backgroundColor: darkGary,
+            ),
       ),
       home: const NotesScreen(),
     );
   }
 }
-
