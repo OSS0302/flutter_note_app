9f86f61cd0e78b3cd4137996cdc35b1bf925e192
ohsangsoo <tkdtn3264@gmail.com>
2023-07-25 18:15:38 +0900

    Di setup

---
+++ lib/di/provider_setup.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_note_app/data/data_source/note_db.dart';
+import 'package:flutter_note_app/data/repository/note_respository_impl.dart';
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
+import 'package:flutter_note_app/presentation/note/notes_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
+Future<List<SingleChildWidget>> getProvider() async{
+  Database database = await openDatabase(
+    'notes_db', // path: notes_db  파일이름 작성
+    version: 1, // 버전은 1로
+    onCreate:(db, version) async{ // db를 비동기로 연다.
+      await db.execute
+        ('CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT , color INTEGER , timestamp, INTEGER)');
+    }
+  );
+  // 객체를 만들어서 전달만 하면된다
+  NoteDbHelper noteDbHelper = NoteDbHelper(database); 
+  NoteRepository repository = NoteRepositoryImpl(noteDbHelper);
+  NotesViewModel notesViewModel = NotesViewModel(repository); // viewMdoel
+  AddEditNoteViewModel addEditNoteViewModel = AddEditNoteViewModel(repository);
+
+  return [ // ChangeNotifier를 통해 변화에 대해 구독할 수 있다.
+    ChangeNotifierProvider(create: (_) => notesViewModel),
+    ChangeNotifierProvider(create: (_) => addEditNoteViewModel),
+  ];
+}
