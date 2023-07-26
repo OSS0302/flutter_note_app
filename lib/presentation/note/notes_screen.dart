160334f799520a00661b625d6db72dcc7b98c079
ohsangsoo <tkdtn3264@gmail.com>
2023-07-26 19:23:10 +0900

    true 일때 뒤로가라

--- lib/presentation/note/notes_screen.dart
+++ lib/presentation/note/notes_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_note_app/domain/model/note.dart';
 import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_screen.dart';
 import 'package:flutter_note_app/presentation/note/components/note_item.dart';
+import 'package:flutter_note_app/presentation/note/notes_event.dart';
 import 'package:flutter_note_app/presentation/note/notes_view_model.dart';
 import 'package:flutter_note_app/ui/colors.dart';
 import 'package:provider/provider.dart';
@@ -29,13 +30,16 @@ class NotesScreen extends StatelessWidget {
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+         bool? isSaved = await Navigator.push( //null 이면 사용자가 뒤로가기 한거고 true 값이 들어오면 saveNote 동작해서 되돌아온거다.
             context,
             MaterialPageRoute(
                 builder: (context) =>
                     const AddEditNoteScreen()), // 첫번째화면에서 두번째화면으로 이동
           );
+         if(isSaved !=null && isSaved ){ // isSaved 널이 아니라면 새로고침 해라
+            viewModel.onEvent(const NotesEvent.loadNotes()); //loadNotes: 새로고침
+         }
         },
         child: const Icon(Icons.add),
       ),
@@ -46,6 +50,7 @@ class NotesScreen extends StatelessWidget {
             children: state.notes
                 .map((note) => NoteItem(
                       note: note,
+
                     ))
                 .toList()),
       ),
