f4675fb51e7d812a00b2c28c3ba4225f34df5e93
ohsangsoo <tkdtn3264@gmail.com>
2023-07-27 13:18:37 +0900

    ui 통해서 처리 했던 스낵바 문구를 봉인클래스 이벤트 로 처리 하기위해서 추가

--- lib/presentation/add_edit_note/add_edit_note_ui_event.dart
+++ lib/presentation/add_edit_note/add_edit_note_ui_event.dart
@@ -5,4 +5,5 @@ part 'add_edit_note_ui_event.freezed.dart';
 @freezed
 abstract class AddEditNoteUiEvent with _$AddEditNoteUiEvent { // 뷰모델에서 이벤트를 ui쪽으로 둘떄 사용하면된다.
   const factory AddEditNoteUiEvent.saveNote() = SaveNote;
+  const factory AddEditNoteUiEvent.showSnackBar(String message) = ShowSnackBar;
 }
