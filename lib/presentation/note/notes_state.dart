e7917d290338420832782e2f659e7b7a1d87ccfe
ohsangsoo <tkdtn3264@gmail.com>
2023-07-27 16:07:34 +0900

    note state에 NoteOrder를 생성해준다.

--- lib/presentation/note/notes_state.dart
+++ lib/presentation/note/notes_state.dart
@@ -1,16 +1,16 @@
 import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/util/note_order.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'notes_state.freezed.dart';
 
-part 'notes_state.g.dart';
 
 @freezed
 class NotesState with _$NotesState {
   factory NotesState({
    required List<Note> notes, // List<Note> notes =[] 를 줄 수없어서 @Default를 사용해서 넣었다.
+   required NoteOrder noteOrder, //NoteOrder 를 추가해주고 나서
   }) = _NotesState;
 
-  factory NotesState.fromJson(Map<String, dynamic> json) => _$NotesStateFromJson(json);
 }
